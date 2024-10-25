2d07c13e99269ac933edc61c03976782809af597
sami.halilou <sami.halilou@etu.uca.fr>
2024-10-25 15:26:35 +0200

    script

--- script.sql
+++ script.sql
@@ -1,61 +1,51 @@
-CREATE TABLE "client" (
-  "id" numeric PRIMARY KEY,
-  "nom" varchar,
-  "prenom" varchar,
-  "mail" varchar,
-  "dateNaiss" date,
-  "tel" varchar,
-  "adresse" varchar,
-  "entreprise" bool,
-  "historique_id" numeric
+CREATE TABLE client (
+  id numeric(4) PRIMARY KEY,
+  nom varchar(50),
+  prenom varchar(50),
+  mail varchar(50),
+  dateNaiss date,
+  tel varchar(15),
+  adresse varchar(255),
+  entreprise boolean,
+  historique_id numeric(6) REFERENCES Historique(id)
 );
 
-CREATE TABLE "voiture" (
-  "id" numeric PRIMARY KEY,
-  "marque" varchar,
-  "modele" varchar,
-  "annee" numeric,
-  "prix_journalier" numeric,
-  "type" varchar,
-  "kilometrage" numeric,
-  "concession_id" numeric,
-  "fournisseur_id" numeric
+CREATE TABLE voiture (
+  id numeric(3) PRIMARY KEY,
+  marque varchar(30),
+  modele varchar(30),
+  annee numeric(4),
+  prix_journalier numeric(3,2),
+  typeVoiture varchar(30),
+  kilometrage numeric(7),
+  concession_id varchar(30) REFERENCES concession(ville),
+  fournisseur_id varchar(30) REFERENCES fournisseur(entreprise)
 );
 
-CREATE TABLE "fournisseur" (
-  "entreprise" varchar PRIMARY KEY,
-  "tel" varchar,
-  "email" varchar,
-  "siege_social" varchar
+CREATE TABLE fournisseur (
+  entreprise varchar(30) PRIMARY KEY,
+  tel varchar(15),
+  email varchar(50),
+  siege_social varchar(30)
 );
 
-CREATE TABLE "concession" (
-  "ville" varchar PRIMARY KEY,
-  "pays" varchar,
-  "nb_employes" numeric
+CREATE TABLE concession (
+  ville varchar(30) PRIMARY KEY,
+  pays varchar(20),
+  nb_employes numeric(2)
 );
 
-CREATE TABLE "location" (
-  "client_id" numeric,
-  "voiture_id" numeric,
-  "debut" date,
-  "fin" date,
-  "coutTotal" numeric,
-  PRIMARY KEY ("client_id", "voiture_id", "debut")
+CREATE TABLE location (
+  client_id numeric REFERENCES client(id),
+  voiture_id numeric REFERENCES voiture(id),
+  debut date,
+  fin date,
+  coutTotal numeric(5,2),
+  PRIMARY KEY (client_id, voiture_id, debut)
 );
 
-CREATE TABLE "Historique" (
-  "id" numeric,
-  "dateInscription" date,
-  "nbrVoitureLoue" numeric
+CREATE TABLE Historique (
+  id numeric(6),
+  dateInscription date,
+  nbrVoitureLoue numeric(3)
 );
-
-ALTER TABLE "location" ADD FOREIGN KEY ("client_id") REFERENCES "client" ("id");
-
-ALTER TABLE "location" ADD FOREIGN KEY ("voiture_id") REFERENCES "voiture" ("id");
-
-ALTER TABLE "voiture" ADD FOREIGN KEY ("concession_id") REFERENCES "concession" ("ville");
-
-ALTER TABLE "voiture" ADD FOREIGN KEY ("fournisseur_id") REFERENCES "fournisseur" ("entreprise");
-
-ALTER TABLE "client" ADD FOREIGN KEY ("historique_id") REFERENCES "Historique" ("id");
